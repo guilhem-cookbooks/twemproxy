99a49dc9787de6f871cd9f6692e4fda628a96147
Guilhem Lettron <guilhem@lettron.fr>
2014-11-04 17:21:43 +0100

    Release 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'guilhem@lettron.fr'
 license 'apache 2.0'
 description 'Installs/Configures twemproxy'
 long_description 'Installs/Configures twemproxy'
-version '0.1.0'
+version '0.1.2'
 
 depends 'apt'
