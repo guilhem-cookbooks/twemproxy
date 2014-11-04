4d74de1ab7c92eafa6b775900ce119ac380b4b27
Guilhem Lettron <guilhem@lettron.fr>
2014-11-04 17:21:01 +0100

    Fix yaml config ugly... I know

--- recipes/config.rb
+++ recipes/config.rb
@@ -15,5 +15,7 @@ file node['twemproxy']['config_file'] do
   owner 'root'
   group 'root'
   mode '0644'
-  content node['twemproxy']['config'].to_yaml
+  # Use JSON to workaround https://tickets.opscode.com/browse/CHEF-3953
+  # Use lines.to_a to suppress first yaml line
+  content JSON.parse(node['twemproxy']['config'].to_json).to_yaml.lines.to_a[1..-1].join
 end
